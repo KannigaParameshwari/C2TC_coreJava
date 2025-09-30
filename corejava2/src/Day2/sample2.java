26229451621e6808eaf17eed03b5f23da327fa9b
Admin <Admin@DESKTOP-5C8IC6G>
2025-09-30 10:35:45 +0530

    this is second sample program

---
+++ corejava2/src/Day2/sample2.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("Main Method");
+		main(20);
+	}
+	public static void main(int a) {
+		System.out.print("Overloaded Main Method" + a);
+	}
+}
